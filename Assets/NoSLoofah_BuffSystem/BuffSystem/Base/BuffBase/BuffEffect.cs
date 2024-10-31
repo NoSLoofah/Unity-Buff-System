e21efc5f19a575e7e26664d2cf6f9e1ebb769f31
haoyizhou <haoyizhou@tencent.com>
2024-10-31 19:40:35 +0800

    fix: 错误的资产创建选项

--- Assets/NoSLoofah_BuffSystem/BuffSystem/Base/BuffBase/BuffEffect.cs
+++ Assets/NoSLoofah_BuffSystem/BuffSystem/Base/BuffBase/BuffEffect.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace NoSLoofah.BuffSystem
-{
-    [CreateAssetMenu(fileName = "NewBuffEffect", menuName = "BuffEffect")]
+{    
     public abstract class BuffEffect : ScriptableObject, IBuffEffect
     {
         public abstract void Fire(Buff sourceBuff);
